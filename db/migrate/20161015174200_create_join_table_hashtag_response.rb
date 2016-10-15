e324117760b1a6b5d51a804048025a9c7324bd72
Victor J Wang <victor@getbuddyapp.com>
2016-10-15 14:57:38 -0400

    add foreign key to responses. remove join table.

--- db/migrate/20161015174200_create_join_table_hashtag_response.rb
+++
@@ -1,9 +0,0 @@
-class CreateJoinTableHashtagResponse < ActiveRecord::Migration[5.0]
-  def change
-    create_join_table :hashtags, :responses do |t|
-      # t.index [:hashtag_id, :response_id]
-      # t.index [:response_id, :hashtag_id]
-			t.timestamps
-    end
-  end
-end
