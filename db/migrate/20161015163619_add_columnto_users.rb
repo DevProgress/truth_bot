83514b1087a61d75a085488827e43c851e669159
Sanjaya Punyasena <sanjaya.punyasena@gmail.com>
2016-10-15 12:41:50 -0400

    add columns

---
+++ db/migrate/20161015163619_add_columnto_users.rb
@@ -0,0 +1,7 @@
+class AddColumntoUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :facebook_id, :string
+    add_column :users, :facebook_token, :string
+    add_column :users, :name, :string
+  end
+end
