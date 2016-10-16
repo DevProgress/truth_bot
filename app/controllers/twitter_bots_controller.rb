05c7f4fb3d770193778f5a368c2fc890ce109b08
Victor J Wang <victor@getbuddyapp.com>
2016-10-16 15:48:01 -0400

    manage twitter bots via ui

--- app/controllers/twitter_bots_controller.rb
+++ app/controllers/twitter_bots_controller.rb
@@ -10,11 +10,25 @@ class TwitterBotsController < ApplicationController
 		@twitter_bot = TwitterBot.find(params[:id])
 	end
 
+	def edit
+		@twitter_bot = TwitterBot.find(params[:id])
+	end
+
 	def create
 		@twitter_bot = TwitterBot.new(twitter_bot_params)
 
 		@twitter_bot.save
 		redirect_to @twitter_bot
+	end
+
+	def update
+		@twitter_bot = TwitterBot.find(params[:id])
+
+		if @twitter_bot.update(twitter_bot_params)
+			redirect_to @twitter_bot
+		else
+			render 'edit'
+		end
 	end
 
 	private
